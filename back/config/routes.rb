51fc693d50d7d4e0793bfc4617e843b8bca7a023
yutaro <plmk5er67@gmail.com>
2025-07-16 02:11:27 +0900

    feat: login and logout

--- back/config/routes.rb
+++ back/config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users, skip: [:registrations]
+  devise_for :users, skip: [:registrations, :sessions]
 
   namespace :api do
     devise_scope :user do
       post 'users/sign_up', to: 'registrations#create'
+      post 'users/sign_in', to: 'sessions#create'
+      delete 'users/sign_out', to: 'sessions#destroy'
     end
   end
 end
--- front/src/lib/api.ts
+++ front/src/lib/api.ts
@@ -1,7 +1,8 @@
-import { RegisterRequest, RegisterResponse } from '@/types/auth';
+import { LoginRequest, LoginResponse, RegisterRequest, RegisterResponse } from '@/types/auth';
 
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
+// 新規登録用
 export async function registerUser(userData: RegisterRequest): Promise<RegisterResponse> {
   const response = await fetch(`${API_BASE_URL}/api/users/sign_up`, {
     method: 'POST',
@@ -14,8 +15,43 @@ export async function registerUser(userData: RegisterRequest): Promise<RegisterR
   const data = await response.json();
 
   if (!response.ok) {
-    throw new Error(data.message || 'ユーザー登録に失敗しました');
+    throw new Error(data.message);
   }
 
   return data;
+}
+
+// ログイン用
+export async function loginUser(userData: LoginRequest): Promise<LoginResponse> {
+  const response = await fetch(`${API_BASE_URL}/api/users/sign_in`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    credentials: 'include',
+    body: JSON.stringify({ user: userData }),
+  });
+
+  const data = await response.json();
+
+  if(!response.ok) {
+    throw new Error(data.message);
+  }
+
+  return data;
+}
+
+// ログアウト用
+export async function logoutUser(): Promise<void> {
+  const response = await fetch(`${API_BASE_URL}/api/users/sign_out`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    credentials: 'include',
+  });
+
+  if(!response.ok){
+    throw new Error('ログアウトに失敗しました');
+  }
 }
