d4860e0636d73adf0bce7ce91c1e407434c25f83
yutaro <plmk5er67@gmail.com>
2025-07-17 22:50:35 +0900

    fix: delete staging setting

--- back/config/initializers/cors.rb
+++ back/config/initializers/cors.rb
@@ -3,8 +3,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     case Rails.env
     when 'production'
       origins 'https://gamut-cut.vercel.app'
-    when 'staging'
-      origins /\Ahttps:\/\/gamut-cut.*\.vercel\.app\z/
     when 'development'
       origins 'http://localhost:3003'
     end
