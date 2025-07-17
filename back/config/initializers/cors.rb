69ec6fedef78c670a756add92e19036f6efbf716
yutaro <plmk5er67@gmail.com>
2025-07-17 20:08:32 +0900

    fix: cors setting

--- back/config/initializers/cors.rb
+++ back/config/initializers/cors.rb
@@ -4,7 +4,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     when 'production'
       origins 'https://gamut-cut.vercel.app'
     when 'staging'
-      origins 'https://gamut-ja554v8jr-yutaros-projects-00a788f9.vercel.app'
+      origins /\Ahttps:\/\/gamut-cut.*\.vercel\.app\z/
     when 'development'
       origins 'http://localhost:3003'
     end
