39952ff1ae283b9703d0ba82e6c2a56c8916f4c1
yutaro <plmk5er67@gmail.com>
2025-08-03 16:31:17 +0900

    add: new_works_column

--- back/app/models/work.rb
+++ back/app/models/work.rb
@@ -9,28 +9,4 @@ class Work < ApplicationRecord
   validates :set_mask_data, presence: true
 
   enum :is_public, { published: 0, restricted: 1, draft: 2 }
-
-  # set_mask_dataの実装（プリセットから独立してマスクデータを保存）
-  # def save_mask_data_from_preset
-  #   if preset.present?
-  #     self.set_mask_data = preset.mask_data
-  #   end
-  # end
-
-  # set_mask_dataのデフォルト値を設定
-  # after_initialize :set_default_mask_data, if: :new_record?
-
-  # private
-
-  # def set_default_mask_data
-  #   self.set_mask_data ||= {
-  #     maskId: 'default_mask',
-  #     maskName: 'デフォルトマスク',
-  #     maskSettings: {
-  #       opacity: 1.0,
-  #       blendMode: 'normal'
-  #     },
-  #     createdAt: Time.current.iso8601
-  #   }
-  # end
 end
