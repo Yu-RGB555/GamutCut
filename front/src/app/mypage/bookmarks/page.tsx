99c91a65a5be0be6b2e27273c0f41d98efd92050
yutaro <plmk5er67@gmail.com>
2025-09-08 23:48:27 +0900

    add: tab_items

---
+++ front/src/app/mypage/bookmarks/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { MyPageLayout } from "@/components/MyPageLayout";
+
+export default function BookmarksPage() {
+  return (
+    <MyPageLayout>
+      <div className="flex justify-center items-center py-8">
+        <div className="text-gray-500">ブックマーク機能は未実装です</div>
+      </div>
+    </MyPageLayout>
+  );
+}
---
+++ front/src/app/mypage/likes/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { MyPageLayout } from "@/components/MyPageLayout";
+
+export default function LikesPage() {
+  return (
+    <MyPageLayout>
+      <div className="flex justify-center items-center py-8">
+        <div className="text-gray-500">いいね機能は未実装です</div>
+      </div>
+    </MyPageLayout>
+  );
+}
