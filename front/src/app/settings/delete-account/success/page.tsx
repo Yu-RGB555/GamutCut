58dc63e37ceced79f5654a0c71c0198128149f6f
yutaro <plmk5er67@gmail.com>
2025-10-28 23:47:53 +0900

    feat: signout

--- front/src/app/settings/delete-account/success/page.tsx
+++ front/src/app/settings/delete-account/success/page.tsx
@@ -5,7 +5,7 @@ import { Card, CardContent } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 import { MdOutlineCheckCircle } from 'react-icons/md';
 
-export default function signOutSuccess() {
+export default function SignOutSuccess() {
   const router = useRouter();
 
     return (
