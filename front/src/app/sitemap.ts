9c8bc15a9e6c7e7c267ec3939c20eca928bb0ff5
yutaro <plmk5er67@gmail.com>
2025-11-27 23:35:27 +0900

    fix: www_domain

--- front/src/app/sitemap.ts
+++ front/src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://gamutcut.com'
+  const baseUrl = 'https://www.gamutcut.com'
 
   return [
     {
