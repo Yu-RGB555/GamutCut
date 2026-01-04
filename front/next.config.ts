97c6a5a2893b54dd691f16aaa75c824caa12710e
yutaro <plmk5er67@gmail.com>
2026-01-04 15:00:33 +0900

    feat: pwa

--- front/next.config.ts
+++ front/next.config.ts
@@ -1,9 +1,20 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
+import withPWAInit from "@ducanh2912/next-pwa"
+
+const withPWA = withPWAInit({
+  dest: "public",
+  register: true,
+  disable: process.env.NODE_ENV === "development",
+  workboxOptions: {
+    skipWaiting: true,
+  }
+});
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
 
 const withNextIntl = createNextIntlPlugin();
-export default withNextIntl(nextConfig);
+
+export default withPWA(withNextIntl(nextConfig));
