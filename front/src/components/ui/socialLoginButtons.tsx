dcccec6d026c34ca1f2e3f284f1bfc3e3ae6bdac
yutaro <plmk5er67@gmail.com>
2026-01-17 14:22:52 +0900

    fix: logo_icon

--- front/src/components/ui/socialLoginButtons.tsx
+++ front/src/components/ui/socialLoginButtons.tsx
@@ -1,4 +1,5 @@
 import { useTranslations } from 'next-intl';
+import Image from 'next/image';
 import React from 'react';
 
 const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
@@ -13,22 +14,26 @@ const SocialLoginButtons: React.FC = () => {
   };
 
   return (
-    <div className="grid gap-4 w-full space-y-3 my-6">
-      <button
+    <div className="flex justify-center w-full gap-x-10 my-6">
+      <Image
+        src="/web_dark_rd_na.svg"
+        alt="Google"
+        width={40}
+        height={40}
         onClick={() => handleSocialLogin('google_oauth2')}
-        className="w-full flex items-center justify-center px-4 py-2.5 border border-gray-300 rounded-sm shadow-sm bg-background text-sm font-medium text-label hover:cursor-pointer"
-      >
-        <img src="https://www.svgrepo.com/show/475656/google-color.svg" alt="Google" className="w-5 h-5 mr-2" />
-        {t('google')}
-      </button>
+        className="hover: cursor-pointer"
+      />
 
-      <button
-        onClick={() => handleSocialLogin('twitter')}
-        className="w-full flex items-center justify-center px-4 py-2.5 border border-gray-300 rounded-sm shadow-sm bg-background text-sm font-medium text-label hover:cursor-pointer"
-      >
-        <img src="/X_logo.svg" alt="X" className="w-5 h-5 mr-2" />
-        {t('X')}
-      </button>
+      <div className="place-items-center border-1 rounded-full p-2">
+        <Image
+          src="/X_logo.svg"
+          alt="X"
+          width={20}
+          height={20}
+          onClick={() => handleSocialLogin('twitter')}
+          className="hover: cursor-pointer"
+        />
+      </div>
     </div>
   );
 };
